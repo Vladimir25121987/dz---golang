483698c5eebd9a444603fe36ef043a057b6a36d8
Vladimir25121987 <KENT_men87@mail.ru>
2024-09-30 21:01:30 +0300

    Модифицированный конвертер валют с map

--- main.go
+++ main.go
@@ -1,106 +1,73 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
-    const UsdToEur = 0.89 
-	const UsdToRub = 91.61
-    var EurToRub = UsdToRub / UsdToEur
-    var EurToUsd = 1 / UsdToEur
-    var RubToUsd = 1 / UsdToRub
-	var RubToEur = 1 / EurToRub
+	const UsdToEur = 0.89
+	const UsdToRub = 92.71
+	type courses map[string]map[string]float64
 
 func main() {
-		var inputCurrency string
-		var outputCurrency string
-		var inputAmount float64
-		var outputAmount float64
-		var err error
-		var amountStr string
-		fmt.Println("Введите сумму конвертации: ")
-		fmt.Scan(&amountStr)
-		amount, err := strconv.ParseFloat(amountStr, 64)
-		if err != nil{
-			fmt.Print("Ошибка: не удалось преобразовать введенное значение в число", err)
-			return
-		}
-		fmt.Printf("Вы ввели число: %2f\n", amount)
+	fmt.Println("КОНВЕРТЕР ВАЛЮТ")
+	courses = courses{
+		"USD": {
+				"EUR": UsdToEur,
+				"RUB": UsdToRub,
+		},
+		"EUR": {
+				"USD": 1 / UsdToEur,
+				"RUB": UsdToEur / UsdToRub,
+		},
+		"RUB": {
+				"USD": 1 / UsdToRub,
+				"EUR": UsdToEur / UsdToRub,
+		},
+	}
+	inputCurrency := getinputСurrency()
+	outputCurrency := getoutputСurrency()
+	inputAmount := getinputAmount()
+	convert(inputAmount, inputCurrency, outputCurrency)
+	fmt.Println()
+}
+func getinputСurrency() string {
+	var incur string
+	fmt.Println("Введите исходную валюту(USD, EUR, RUB): ")
+	for {
+		fmt.Scan(&incur)
+			if incur != "USD" || incur != "EUR" || incur != "RUB"{
+		fmt.Println("Вы неправильно ввели исходную валюту. Повторите ввод.")
+	} 
+	return incur
+	}	
+}
 
+func getoutputСurrency(inputCurrency string) string {
+	var outcur string
+	fmt.Println("Выберите желаемую валюту(USD, EUR, RUB): ")
 	for {
-			inputCurrency, err = scanCurrancy("Введите исходную валюту(EUR, USD, RUB): ")
-		if err != nil {
-			fmt.Println(err)
-			continue
+		fmt.Scan(&outcur)
+		if outcur != "USD" || outcur != "EUR" || outcur != "RUB"{
+			fmt.Println("Вы неправильно ввели желаемую валюту. Повторите ввод.")
+		} 
+		if outcur == inputCurrency{
+			fmt.Println("Исходная и желаемая валюты совпадают. Повторите ввод.")
 		}
-			break
+		return outcur
 	}
+}
+
+func getinputAmount() float64 {
+	var sum float64
+	fmt.Println("Выберите сумму конвертации: ")
 	for {
-			inputAmount, err = scanAmount("Введите сумму конвертации: ")
-		if err != nil {
-			fmt.Println(err)
-			continue
+		fmt.Scan(&sum)
+		if sum < 0 {
+			fmt.Println("Ошибка. Введите положительное число")
 		}
-			break
-	}
-	for {
-			outputCurrency, err = scanCurrancy("Введите целевцю валюту(EUR, USD, RUB):  ")
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-			break
-	}
+		return sum
+	}	
+}
 	
-		outputAmount = convert(inputAmount, inputCurrency, outputCurrency)
-		fmt.Scanf("%.2f", outputAmount)
-}
-
-func scanCurrancy(prefixPrint string) (string, error) {
-	var currency string
-	fmt.Print("prefixPrint")
-	fmt.Scanln(&currency)
-	if currency != "USD" && currency != "EUR" && currency != "RUB"{
-		return "",errors.New("WRONG_CURRENCY")
-	}
-	return currency, nil
-}
-
-func scanAmount(prefixPrint float64) (float64, error) {
-	var amount float64
-	fmt.Print("prefixPrint")
-	fmt.Scanln(&amount)
-	if amount < 0 {
-return 0, errors.New("Введено значение меньше 0")
-	}
-	return amount, nil
-}
-
-func convert(inputAmount float64, inputCurrency string, outputCurrency string) float64 {
-switch inputCurrency {
-case "USD":
-	if outputCurrency == "EUR"{
-		return inputAmount * UsdToEur
-	}
-	if outputCurrency == "RUB"{
-		return inputAmount * UsdToRub
-	}
-case "EUR":
-	if outputCurrency == "USD"{
-		return inputAmount * EurToUsd
-	}
-	if outputCurrency == "RUB"{
-		return inputAmount * EurToRub
-	}
-case "RUB":
-	if outputCurrency == "EUR"{
-		return inputAmount * RubToEur
-	}
-	if outputCurrency == "USD"{
-		return inputAmount * RubToUsd
-	}
-  }
-return inputAmount
+func convert(courses courses , inputAmount float64, inputCurrency string, outputCurrency string) float64 {
+	result = inputAmount * courses[inputCurrency][outputCurrency]
+return result
 }
