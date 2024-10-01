615e2df9baa567f0563bec640c43722b66f19657
Vladimir25121987 <KENT_men87@mail.ru>
2024-10-01 21:30:29 +0300

    Исправлены ошибки в конвертере с map

--- main.go
+++ main.go
@@ -2,72 +2,75 @@ package main
 
 import "fmt"
 
-	const UsdToEur = 0.89
-	const UsdToRub = 92.71
-	type courses map[string]map[string]float64
+const UsdToEur = 0.9 
+const UsdToRub = 93.36
 
-func main() {
-	fmt.Println("КОНВЕРТЕР ВАЛЮТ")
-	courses = courses{
-		"USD": {
-				"EUR": UsdToEur,
-				"RUB": UsdToRub,
-		},
-		"EUR": {
-				"USD": 1 / UsdToEur,
-				"RUB": UsdToEur / UsdToRub,
-		},
-		"RUB": {
-				"USD": 1 / UsdToRub,
-				"EUR": UsdToEur / UsdToRub,
+type courses map[string]map[string]float64
+
+func main() { 
+fmt.Println("КОНВЕРТЕР ВАЛЮТ")
+courses := courses {  
+		"USD": {    
+			"EUR": UsdToEur,   
+			"RUB": UsdToRub,  
+		},  
+		"EUR": {  
+			"USD": 1 / UsdToEur,   
+			"RUB": UsdToEur / UsdToRub, 
+		},  
+		"RUB": { 
+			"USD": 1 / UsdToRub,  
+			"EUR": UsdToEur / UsdToRub,  
 		},
 	}
-	inputCurrency := getinputСurrency()
-	outputCurrency := getoutputСurrency()
-	inputAmount := getinputAmount()
-	convert(inputAmount, inputCurrency, outputCurrency)
-	fmt.Println()
-}
-func getinputСurrency() string {
-	var incur string
-	fmt.Println("Введите исходную валюту(USD, EUR, RUB): ")
-	for {
-		fmt.Scan(&incur)
-			if incur != "USD" || incur != "EUR" || incur != "RUB"{
-		fmt.Println("Вы неправильно ввели исходную валюту. Повторите ввод.")
-	} 
-	return incur
-	}	
+
+inputCurrency := getInputCurrency() 
+outputCurrency := getOutputCurrency(inputCurrency) 
+inputAmount := getInputAmount() 
+convertedAmount := convert(courses, inputAmount, inputCurrency, outputCurrency) 
+fmt.Printf("%.2f %s = %.2f %s\n", inputAmount, inputCurrency, convertedAmount, outputCurrency) 
 }
 
-func getoutputСurrency(inputCurrency string) string {
-	var outcur string
-	fmt.Println("Выберите желаемую валюту(USD, EUR, RUB): ")
-	for {
-		fmt.Scan(&outcur)
-		if outcur != "USD" || outcur != "EUR" || outcur != "RUB"{
-			fmt.Println("Вы неправильно ввели желаемую валюту. Повторите ввод.")
+func getInputCurrency() string {
+	 var incur string 
+	fmt.Println("Введите исходную валюту(USD, EUR, RUB): ") 
+	for {  
+		 fmt.Scan(&incur)  
+		  if incur == "USD" || incur =="EUR" || incur == "RUB" { 
+			   return incur  
+			 } 
+		    fmt.Println("Вы неправельно ввели исходную валюту. Повторите ввод.")
 		} 
-		if outcur == inputCurrency{
-			fmt.Println("Исходная и желаемая валюты совпадают. Повторите ввод.")
-		}
-		return outcur
-	}
 }
 
-func getinputAmount() float64 {
+func getOutputCurrency(inputCurrency string) string { 
+	var outcur string 
+	fmt.Println("Выберите желаемую валюту(USD, EUR, RUB): ") 
+	for {  
+		fmt.Scan(&outcur)  
+		if outcur == "USD" || outcur == "EUR" || outcur == "RUB" {    
+		if outcur != inputCurrency {   
+			  return outcur    
+			  }  
+			    fmt.Println("Исходная и желаемая валюты совпадают. Повторите ввод.")  
+		} 
+				fmt.Println("Вы неправельно ввели желаемую валюту. Повторите ввод.") 
+	} 
+}
+
+func getInputAmount() float64 { 
 	var sum float64
-	fmt.Println("Выберите сумму конвертации: ")
-	for {
-		fmt.Scan(&sum)
-		if sum < 0 {
-			fmt.Println("Ошибка. Введите положительное число")
-		}
-		return sum
-	}	
+	  	fmt.Println("Введите сумму конвертации: ")
+	for {   
+		fmt.Scan(&sum) 
+		if sum >= 0 {   
+			 return sum   
+			 } 
+		fmt.Println("Ошибка. Введите положительное число") 
+		} 
 }
-	
-func convert(courses courses , inputAmount float64, inputCurrency string, outputCurrency string) float64 {
-	result = inputAmount * courses[inputCurrency][outputCurrency]
-return result
-}
+
+func convert(courses courses, inputAmount float64, inputCurrency string, outputCurrency string) float64 {
+	 result := inputAmount * courses[inputCurrency][outputCurrency]
+	  return result
+ }
